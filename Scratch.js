c33eaec9c3b8ce4cc3f29ca9da1020802d407c0e
Roman <rp32@gmail.com>
2025-10-21 22:16:08 -0400

    Add table-sort assignment with sort, birth, and shuffle buttons

--- Scratch.js
+++ Scratch.js
@@ -6,3 +6,19 @@ git push */
 
 console.log("Rama".length)
 
+
+
+
+Array.reduce((soFar,next)=> soFar.concat(next))
+
+
+
+
+const mySetWithValues = new Set([1, 2, 3, 4, 5]);
+console.log(mySetWithValues);
+mySetWithValues.add(10);
+console.log(mySetWithValues);
+mySetWithValues.add(1);
+console.log(mySetWithValues);
+
+
